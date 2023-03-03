1a02da3849e0ded95a7cd1834ca9f9add83dac27
Ines-Triki <inestriki226@gmail.com>
2023-03-03 10:54:29 +0100

    done

--- style.js
+++ style.js
@@ -19,7 +19,7 @@ var verification = setInterval(function(){
 
     if(obstaclesLeft<20 && obstaclesLeft >0 && persoTop>= 130){
         obstacles.style.animation = "none";
-        alert("Vous avez perdu")
+        alert("game over")
 
     }
 
